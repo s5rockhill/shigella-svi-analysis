3bb62e00ca1e04702927d12644adc7f4e4a0bc73
Sarah Rockhill <ppk8@cdc.gov>
2023-07-03 14:12:05 -0400

    removed data.table functions and replaced with dplyr

--- data cleaning/county-incidence-regression-data.R
+++ data cleaning/county-incidence-regression-data.R
@@ -1,53 +1,55 @@
-library('data.table')
+library('dplyr')
 library('stringr')
 library('openxlsx')
-library('RODBC')
 library('tigris')
+library('sf')
 
 #-------------------------------------------------------------------------------
-#Import shigella case data and create aggregation fields
+#Import shigella case data 
 #-------------------------------------------------------------------------------
 folder<-'//cdc.gov/project/ATS_GIS_Store4/Projects/prj06135_Shigella_SVI/Data/Final Datasets/'
 
-dat<-setDT(read.csv(paste0(folder, "analytic_file_final_06062023.csv"), stringsAsFactors = F,
-                    colClasses = c('CTNO2000'='character', 'CTNO2010'='character')))
+dat<-read.csv(paste0(folder, "analytic_file_final_06162023.csv"), stringsAsFactors = F,
+        colClasses = c('CTNO2000'='character', 'CTNO2010'='character', 
+                       'Deterministic'='character', 'CNTYFIPS'='character'))
 
-#Classify age
+#-------------------------------------------------------------------------------
+#Create aggregation fields
+#Note: American Indian/Alaska Native is top-coded
+#-------------------------------------------------------------------------------
 AgeGroupLabs<-c('0-4', '5-14', '15-44', '45+')
-dat$AgeRange<-cut(dat$Age, breaks=c(0, 5, 15, 45, 999), right=F, labels=AgeGroupLabs)
-
-#Update missing census tract information using deterministic imputation result
-dat[is.na(CTNO2000), CTNO2000 := Deterministic]
-dat[is.na(CTNO2010), CTNO2010 := Deterministic]
-dat$CTNO2000<-str_pad(dat$CTNO2000, width=11, side='left', pad='0')
-
-#Classify cases by which year of svi we want to join to 
 svibreaks<-c(2000, 2006, 2011, 2015, 2017, 2020)
 svilabels<-c('2000', '2010', '2014', '2016', '2018')
-dat[,'sviyear' := cut(Year, breaks=svibreaks, right=F, labels=svilabels)]
 
-#Classify county based on census tract GEOID if available and otherwise based on foodnet county
-data(fips_codes)
-fips_codes$county<-toupper(gsub(' County',"", fips_codes$county))
-dat$County[dat$County=='PRINCE GEORGES']<-"PRINCE GEORGE'S"
 dat <- dat %>%
-  left_join(fips_codes, join_by(State==state, County==county)) %>%
-  mutate(GEOID=ifelse(Year<2006, str_sub(CTNO2000, 1, 5), str_sub(CTNO2010, 1, 5))) %>%
-  mutate(GEOID=ifelse(is.na(GEOID), paste0(state_code, county_code), GEOID))
+  mutate(AgeRange=cut(Age, breaks=c(0, 5, 15, 45, 999), right=F, labels=AgeGroupLabs),
+         raceeth= ifelse(RacEthGroupA %in% c('Asian-NH', 'NatHwn-PI'), 'Asian, NH and PI',  
+                    ifelse(RacEthGroupA %in% c('Multiracial-NH', 'Other-NH', 'Unknown'),  'Other-Unknown-multi', 
+                                RacEthGroupA)),
+         sviyear=cut(Year, breaks=svibreaks, right=F, labels=svilabels))
 
-#Classify race and ethnicity (top-coding american indian)
-dat<- dat %>%
-  mutate(raceeth= ifelse(RacEthGroupA %in% c('Asian-NH', 'NatHwn-PI'), 'Asian, NH and PI',  
-                  ifelse(RacEthGroupA %in% c('Multiracial-NH', 'Other-NH', 'Unknown'),  'Other-Unknown-multi', 
-                  RacEthGroupA)))
+#-------------------------------------------------------------------------------
+#Classify county based on CNTYFIPS if available and otherwise based on foodnet 
+#county. Adds land area for population density calculation
+#-------------------------------------------------------------------------------
+counties<-counties(cb=T) %>%
+  st_drop_geometry() %>%
+  select(GEOID, NAME, STUSPS, ALAND) %>%
+  mutate(NAME=ifelse(GEOID=='24510', 'BALTIMORE CITY', 
+            toupper(stringi::stri_trans_general(NAME, "Latin-ASCII"))))
+
+dat <- dat %>%
+  mutate(County=ifelse(County=='PRINCE GEORGES', "PRINCE GEORGE'S", County)) %>%
+  left_join(counties, join_by(State==STUSPS, County==NAME)) %>%
+  mutate(CNTYFIPS=ifelse(is.na(CNTYFIPS), GEOID, CNTYFIPS))
 
 #-------------------------------------------------------------------------------
 # Sum cases by year, county, age group, and race/ethnicity category 
 #-------------------------------------------------------------------------------
-CaseCounts<-dat[, list(Cases=.N), by=list(sviyear, GEOID, AgeRange, raceeth)]
-
-#Drop unknown, multiracial and unknown race and missing age 
-CaseCounts<-subset(CaseCounts, raceeth != 'Other-Unknown-multi' & !is.na(AgeRange))
+CaseCounts<-dat %>%
+  group_by(sviyear, CNTYFIPS, AgeRange, raceeth) %>%
+  summarise(Cases=n()) %>%
+  filter(raceeth != 'Other-Unknown-multi' & !is.na(AgeRange))
 
 #-------------------------------------------------------------------------------
 # Import 2004-2019 County population data for foodnet surveillance area
@@ -55,39 +57,70 @@ CaseCounts<-subset(CaseCounts, raceeth != 'Other-Unknown-multi' & !is.na(AgeRang
 folder<-'//cdc.gov/project/ATS_GIS_Store4/Projects/prj06135_Shigella_SVI/Data/FoodNet Population Data/'
 
 pop<-read.csv(paste0(folder, 'nchs_pop_est_by_county_aian_topcode.csv'), 
-                      stringsAsFactors = F, colClasses = c("GEOID"='character'))
+             stringsAsFactors = F, colClasses = c("GEOID"='character'))
 #-------------------------------------------------------------------------------
 # Merge population and case counts. Executing a left join to keep tracts with 0 cases. 
 #-------------------------------------------------------------------------------
-pop$sviyear<-as.factor(pop$sviyear)
-fin<-merge(pop, CaseCounts, by = c('sviyear', 'GEOID', 'AgeRange', 'raceeth'), all.x=T)
-fin$Cases[is.na(fin$Cases)]<-0
-
-#Classify state
-fin<-fin %>%
-  mutate(State=factor(str_sub(GEOID, 1, 2), 
-                levels=c('06','08','09','13','24','27','35','36','41','47'),
-                 labels=c('CA', 'CO', 'CT', 'GA', 'MD', 'MN', 'NM', 'NY', 'OR', 'TN')))
+CaseCounts$sviyear<-as.numeric(as.character(CaseCounts$sviyear))
+fin<-pop %>%
+  rename(CNTYFIPS=GEOID) %>%
+  left_join(CaseCounts, join_by(sviyear, CNTYFIPS, AgeRange, raceeth)) %>%
+   mutate(Cases=tidyr::replace_na(Cases, 0))
 
 #-------------------------------------------------------------------------------
-#Import NCHS Urban-Rural classification and merge to data by STCNTY
+#Import NCHS Urban-Rural classification and merge to final dataset
 #-------------------------------------------------------------------------------
 folder<-'//cdc.gov/project/ATS_GIS_Store4/Projects/prj06135_Shigella_SVI/Data/NCHS/'
 nchs<-read.xlsx(paste0(folder, 'NCHSURCodes2013.xlsx'))
-nchs$GEOID<-str_pad(nchs$FIPS.code, 5, "left", "0")
+nchs$CNTYFIPS<-str_pad(nchs$FIPS.code, 5, "left", "0")
 nchs$UrCode<-nchs$`2013.code`
 
-fin<-merge(fin, nchs[,10:11], by='GEOID', all.x=T)
-names(fin)[names(fin)=='GEOID']<-'County'
+fin<-fin %>%
+  left_join(nchs[,10:11], join_by(CNTYFIPS)) %>%
+  rename(County=CNTYFIPS)
+            
+#--------------------------------------------------------------------------------
+# Merge in additional SDOH covariates: number of childcare businesses and
+# temporary shelters, and healthcare provider shortage area designation
+#--------------------------------------------------------------------------------
+source('helper funcs/county_business_patterns.R')
+
+fin <- fin %>%
+  left_join(cbp, join_by(County, sviyear==cbp_year))
+
+source('helper funcs/ahrq_sdoh_hpsa.R')
+
+fin <- fin %>%
+  left_join(sdoh, join_by(County, sviyear))
 
 #-------------------------------------------------------------------------------
 # Merge to 2000-2018 SVI scores for FoodNet Counties 
 #-------------------------------------------------------------------------------
 source('helper funcs/calc-region-specific-svi-score-county.R')
 
-fin$sviyear<-as.numeric(as.character(fin$sviyear))
 fin<-fin %>%
-  left_join(svi_all, join_by(County==County, sviyear==Year))
+  left_join(svi_all, join_by(County, sviyear==Year))
+
+#-------------------------------------------------------------------------------
+# Merge land area population density
+#-------------------------------------------------------------------------------
+density<-pop %>%
+    group_by(GEOID, sviyear) %>%
+    summarise(Pop=sum(Pop)) %>%
+    left_join(counties[, c('GEOID', 'ALAND')], join_by(GEOID)) %>%
+    mutate(PopDensity = Pop/(ALAND/1000000)) %>%
+    select(GEOID, sviyear, ALAND, PopDensity)
+
+fin <- fin %>%
+  left_join(density, join_by(County==GEOID, sviyear))
+
+#-------------------------------------------------------------------------------
+# Merge in HIFLD prison data
+#-------------------------------------------------------------------------------
+source('helper funcs/hifld_prisons.R')
+
+fin <- fin %>%
+  left_join(prisons_agg, join_by(County))
 
 ########################## END DATA PREPARATION  #####################################
 
@@ -97,6 +130,3 @@ fin<-fin %>%
 output.location<-c("//cdc.gov/project/ATS_GIS_Store4/Projects/prj06135_Shigella_SVI/Data/Final Datasets/Final_County_")
 
 write.csv(fin, paste0(output.location, "ByRaceEth_", Sys.Date(), '.csv'), row.names=F)
-
-
-
